2af84e6eed473697d7c029e326c6088007079a3b
Sanketh Nayak <160486607+Sanketh159@users.noreply.github.com>
2024-11-02 10:50:53 +0530

    Update Projects.jsx

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import "../public/projects.css";
+import foodclone from "../src/assets/Images/foodman.png";
+import calculator from "../src/assets/Images/calculator.png";
+import spotify from "../src/assets/Images/spotify.png";
 
 function ProjectSection() {
   const projects = [
     {
       title: "Food Clone",
-      image: "../src/assets/Images/foodman.png",
+      image: foodclone,
       desc: "I Built a static food clone page using HTML, CSS and java script. This clone contains  Navbar,different section and it is Responsive Page. This page suitable for both pc and moblie ",
       link: "https://foodmanapp.netlify.app",
       glink:'https://github.com/Sanketh159/food-page.git'
     },
     {
       title: "Calculator",
-      image: "../src/assets/Images/calculator.png",
+      image: calculator,
       desc: "I created Simple calculator which perfoms Arithmatic operation.",
       link: "https://harmonious-praline-557791.netlify.app/",
       glink:'https://github.com/Sanketh159/codsoft.calcualator.git'
     },
     {
       title: "Spotify Clone",
-      image: "../src/assets/Images/spotify.png",
+      image: spotify,
       desc: "I created spotify's Home page using HTML and CSS ",
       link: "https://sparkly-alfajores-188b55.netlify.app/",
       glink: "https://github.com/Sanketh159/spotify-clone.git"
