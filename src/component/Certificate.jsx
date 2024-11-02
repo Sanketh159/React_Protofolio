e7bc770a12e078acdf3c9dc82190dd36ea957e49
Sanketh Nayak <160486607+Sanketh159@users.noreply.github.com>
2024-11-02 10:46:49 +0530

    Update Certificate.jsx

--- src/component/Certificate.jsx
+++ src/component/Certificate.jsx
@@ -1,29 +1,35 @@
 import React from "react";
-import "../public/certificate.css"; // Ensure to link your CSS file
+import "../public/certificate.css";
+import reactAndJs from '../assets/certificate/reactandjs.jpg';
+import aws from '../assets/certificate/aws.jpg';
+import problemSolving from '../assets/certificate/problem.png';
+import sql from '../assets/certificate/sql(Basic).png';
+// Ensure to link your CSS file
 
 function CertificateSection() {
   const certificates = [
-    {
-      title: "React and JS",
-      image: "../src/assets/certificate/reactandjs.jpg",
-      link: "https://drive.google.com/file/d/1iKZIdlXAiYXLZ5MeZnPz1oo1mOYn5QHQ/view?usp=drive_link",
-    },
-    {
-      title: "AWS for React",
-      image: "../src/assets/certificate/aws.jpg",
-      link: "https://drive.google.com/file/d/1ovx6r7Cglb36IZcVngKK2ELgyqd0DORB/view?usp=drive_link",
-    },
-    {
-      title: "Problem Solving (Basic)",
-      image: "../src/assets/certificate/problem.png",
-      link: "https://drive.google.com/file/d/17uiWtVtvdhF7nraQRnv81Krgyl044lrE/view?usp=drive_link",
-    },
-    {
-      title: "SQL (Basic)",
-      image: "../src/assets/certificate/sql(Basic).png",
-      link: "https://drive.google.com/file/d/1haxSdqWKpp6LbYm8gYTgGRuxQovi__C6/view?usp=drive_link",
-    },
-  ];
+  {
+    title: "React and JS",
+    image: reactAndJs,
+    link: "https://drive.google.com/file/d/1iKZIdlXAiYXLZ5MeZnPz1oo1mOYn5QHQ/view?usp=drive_link",
+  },
+  {
+    title: "AWS for React",
+    image: aws,
+    link: "https://drive.google.com/file/d/1ovx6r7Cglb36IZcVngKK2ELgyqd0DORB/view?usp=drive_link",
+  },
+  {
+    title: "Problem Solving (Basic)",
+    image: problemSolving,
+    link: "https://drive.google.com/file/d/17uiWtVtvdhF7nraQRnv81Krgyl044lrE/view?usp=drive_link",
+  },
+  {
+    title: "SQL (Basic)",
+    image: sql,
+    link: "https://drive.google.com/file/d/1haxSdqWKpp6LbYm8gYTgGRuxQovi__C6/view?usp=drive_link",
+  },
+];
+
 
   return (
     <section id="wrapper">
