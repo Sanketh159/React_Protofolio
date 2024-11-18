51df4f7fbe6d762250d97890189090cc84030556
Sanketh159 <sankethsnayak1592gmail.com>
2024-11-18 18:39:26 +0530

    added contact footer

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -3,7 +3,18 @@ import '../public/Contact.css'
 
 function Contact() {
   return (
-    <></>
+    <>
+      <footer>
+        <section id="contact">
+            <h2 id="details">Contanct Details</h2>
+            <div class="contact">
+                <i class="" style="color: lightblue;"></i> +917204356073 
+                <p style="border-left:1px solid white; padding-left:20px;"> <i class="fa-solid fa-envelope" style="color:blanchedalmond;"></i><a
+                    href="mailto:sankethsnayak159@gmail.com" style="color:#fff;">&nbsp;&nbsp;sankethsnayak159@gmail.com</a></p>
+            </div>
+        </section>
+    </footer>
+    </>
   )
 };
 
